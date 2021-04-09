4c5ff272f6deeb11e04ea26e52c3a749c75d7983
Max Schwarz <max.schwarz@ais.uni-bonn.de>
2021-04-09 11:09:05 +0200

    .github: windows: curl

--- .github/actions/windows/run.sh
+++ .github/actions/windows/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-wget -O basic-miktex-x64.exe https://miktex.org/download/win/basic-miktex-x64.exe
+curl -o basic-miktex-x64.exe https://miktex.org/download/win/basic-miktex-x64.exe
 ./basic-miktex-x64.exe --unattended --auto-install=yes --shared --package-set=basic
 echo "Done!"
 
