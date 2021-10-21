c465aa48c36e2fa13e95c114d26cdb208a44870c
Max Schwarz <max.schwarz@ais.uni-bonn.de>
2021-10-21 17:25:31 +0200

    ci: actually test with -shell-escape -.-

--- .github/actions/ubuntu16/entrypoint.sh
+++ .github/actions/ubuntu16/entrypoint.sh
@@ -17,9 +17,19 @@ TEXINPUTS=..: lualatex -interaction=nonstopmode paper
 echo "Clearing cache..."
 rm -Rf graphicscache
 
+echo "Compiling example using pdflatex with shell-escape"
+TEXINPUTS=..: pdflatex -interaction=nonstopmode -shell-escape paper
+
+echo "Compiling example using lualatex with shell-escape"
+rm -f paper.aux
+TEXINPUTS=..: lualatex -interaction=nonstopmode -shell-escape paper
+
+echo "Clearing cache..."
+rm -Rf graphicscache
+
 echo "Compiling nonstandard_cachedir using pdflatex"
-TEXINPUTS=..: pdflatex -interaction=nonstopmode nonstandard_cachedir
+TEXINPUTS=..: pdflatex -interaction=nonstopmode -shell-escape nonstandard_cachedir
 
 echo "Compiling nonstandard_cachedir using lualatex"
 rm -f paper.aux
-TEXINPUTS=..: lualatex -interaction=nonstopmode nonstandard_cachedir
+TEXINPUTS=..: lualatex -interaction=nonstopmode -shell-escape nonstandard_cachedir
--- .github/actions/ubuntu20/entrypoint.sh
+++ .github/actions/ubuntu20/entrypoint.sh
@@ -23,13 +23,26 @@ echo "::group::Clearing cache..."
 rm -Rf graphicscache
 echo "::endgroup::"
 
+echo "::group::Compiling example using pdflatex with shell-escape"
+TEXINPUTS=..: pdflatex -interaction=nonstopmode -shell-escape paper
+echo "::endgroup::"
+
+echo "::group::Compiling example using lualatex with shell-escape"
+rm -f paper.aux
+TEXINPUTS=..: lualatex -interaction=nonstopmode -shell-escape paper
+echo "::endgroup::"
+
+echo "::group::Clearing cache..."
+rm -Rf graphicscache
+echo "::endgroup::"
+
 echo "::group::Compiling nonstandard_cachedir using pdflatex"
-TEXINPUTS=..: pdflatex -interaction=nonstopmode nonstandard_cachedir
+TEXINPUTS=..: pdflatex -interaction=nonstopmode -shell-escape nonstandard_cachedir
 echo "::endgroup::"
 
 echo "::group::Compiling nonstandard_cachedir using lualatex"
 rm -f paper.aux
-TEXINPUTS=..: lualatex -interaction=nonstopmode nonstandard_cachedir
+TEXINPUTS=..: lualatex -interaction=nonstopmode -shell-escape nonstandard_cachedir
 echo "::endgroup::"
 )
 
